568a66562e32a9b0e98d1e821188a321b540dcab
Mark <lemay@bu.edu>
2016-11-06 18:11:35 -0500

    remove support for generators

--- byterun/pyvm2.py
+++ byterun/pyvm2.py
@@ -32,7 +32,7 @@ from typing import Optional, Dict, Any, Tuple, Iterable, Union, List
 import six
 from six.moves import reprlib
 
-from .pyobj import Frame, Block, Method, Function# , Generator
+from .pyobj import Frame, Block, Method, Function
 
 log = logging.getLogger(__name__)
 
